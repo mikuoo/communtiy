3f83cbeabf05ebb32122d8a3df1d7f2b2f5c852c
mikucc <2149771396@qq.com>
2021-11-30 22:30:20 +0800

    init commit

---
+++ src/main/java/com/zch/communtiy/communtiy/Controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.zch.communtiy.communtiy.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: zch
+ * @Date: 2021/11/30 22:04
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
---
+++ src/test/java/com/zch/communtiy/communtiy/CommuntiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zch.communtiy.communtiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuntiyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
